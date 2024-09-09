67efa9d1f5884f7cd6d2d2e84c17e2c6f79145c1
Zamza584 <mzamalloa97@gmail.com>
2024-09-08 22:25:43 -0500

    font fix 2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link href='https://fonts.googleapis.com/css2?family=Libre+Caslon+Text:ital,wght@0,400;0,700;1,400&display=swap' rel='stylesheet' type='text/css' />
+        <link href='https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap' rel='stylesheet' type='text/css' />
 
       </head>
       <body className={inter.className} suppressHydrationWarning={true}>
